e6b627da3f4d0b5bb146fb4b810db85866712391
Cowfresh <51661234+Cowfresh@users.noreply.github.com>
2021-08-27 14:22:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ const getTopShows = async (actorId) => {
       for (let shows of castCredRes.data) {
         if (castCredRes.data.indexOf(shows) < 3) {
 	  if (castCredRes.data.indexOf(shows) > 1) {
-		  showsCont.style.justifyContent = 'space-between'
+		  showsCont.style.justifyContent = 'space-around'
 	  }
           const showDiv = document.createElement("DIV");
           const showTitle = document.createElement("H2");
